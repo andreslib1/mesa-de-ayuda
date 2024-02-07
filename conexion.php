daa25a5c292ba375428260150762509a16ce96d6
andreslib1 <andreslib8@gmail.com>
2024-02-06 21:45:05 -0500

    se corrije readme y se elimina archivos

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db = mysqli_connect('82.180.138.204', 'u253606672_nexoweblabs', '+oF^rKg*;dv3t]xG', 'u253606672_db1_proyectos');
-
-if (!$db){
-    echo "Error al conectar a la base de datos";
-    exit;
-}
-
